acd6cd0b1b86c513d1b0c2fd3fa33a3d20836d7e
Miguel Murillo <miguel_murillo@epam.com>
2017-10-10 14:16:29 -0500

    Added scraper

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const fs = require("fs");
+const request = require("request");
+const cheerio = require("cheerio");
+const app = express();
+
+app.get("/scrape", function(req, res) {
+  url = "https://www.easports.com/us/fifa/news/2017/fifa-18-leagues-and-teams";
+  request(url, function(error, response, html) {
+    if (!error) {
+      const $ = cheerio.load(html);
+      const json = { title: "", teams: [] };
+      let title, teams;
+      $(".eas-b2 > div:nth-child(5)").filter(function() {
+        const data = $(this);
+        //title = data.find('b').text();
+        const teams = [];
+        data.find("ul > li").each(function(i, elem) {
+          teams[i] = $(this).text();
+        });
+        teams.sort();
+        teams.forEach(function(elem, i) {
+          json.teams.push({ 'name': elem });
+        });
+        res.send(json);
+      });
+
+      fs.writeFile("output.json", JSON.stringify(json, null, 4), function(err) {
+        console.log(
+          "File successfully written! - Check your project directory for the output.json file"
+        );
+      });
+    }
+  });
+});
+
+app.listen("8081");
+
+console.log("Magic happens on port 8081");
+
+exports = module.exports = app;
